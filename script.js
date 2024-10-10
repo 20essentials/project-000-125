0c477c0c99f7bd80d39f7a3f9f39ca0550a8f457
20essentials <s0l0estudi4r@gmail.com>
2024-10-10 13:32:49 +0200

    v0.0.1

---
+++ script.js
@@ -0,0 +1,14 @@
+console.groupCollapsed('Reference ⬇️');
+console.log(
+  'Joker card Image',
+  'https://posterstore.com/p/posters-prints/the-joker-the-joker-vs-batman-poster/'
+);
+console.log(
+  'Card hover Effect, Online Tutorials: ',
+  'https://youtu.be/H-ScHKhXZVM?si=kbkeMmBe5dnxgn-v'
+);
+console.log(
+  'Texture Batman',
+  'https://www.vecteezy.com/vector-art/12069271-set-of-bats-silhouettes-flying'
+);
+console.groupEnd('');
